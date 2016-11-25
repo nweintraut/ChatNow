3dc244496e65cc2f922b6f8fc708c6c68f2e2e40
Neil Weintraut <neil@Neils-iMac.local>
2016-11-24 22:38:03 -0800

    Created Container

--- components/App.js
+++ components/App.js
@@ -7,14 +7,14 @@ import {
 
 import routes from '../routes'
 import MainScreen from './MainScreen'
-import SignInScreen from './SignInScreen'
+import SignInContainer from '../containers/SignInContainer'
 import NavBarRouteMapper from './NavBarRouteMapper'
 
 class App extends Component {
 	_renderScene(route, navigator) {
 		switch(route.name) {
 			case 'SignInScreen':
-				return <SignInScreen />
+				return <SignInContainer />
 			case 'MainScreen':
 				return <MainScreen getHelpPressHandler={() => {
 					navigator.push(routes.signIn)
